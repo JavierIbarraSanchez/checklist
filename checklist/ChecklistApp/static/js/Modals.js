03338c9453a1f108ef535d6fe6de5f6fc4002bdb
JavierIbarraSanchez <javier.i.ibarra.s@gmail.com>
2022-02-07 17:36:31 -0300

    se agregan los css

---
+++ checklist/ChecklistApp/static/js/Modals.js
@@ -0,0 +1,37 @@
+
+function abrir_modal_edit_check(url){
+
+    $('#editar_checklist').load(url, function(){
+        $(this).modal('show');
+    })
+}
+
+function abrir_modal_eliminar_check(url){
+
+$('#modalEliminar').load(url, function(){
+$(this).modal('show');
+})
+}
+
+
+function abrir_modal_crear_act(url){
+
+    $('#modalCrear').load(url, function(){
+        $(this).modal('show');
+    })
+}
+
+
+function abrir_modal_editar_act(url){
+
+    $('#modalEditar').load(url, function(){
+        $(this).modal('show');
+    })
+}
+
+function abrir_modal_eliminar_act(url){
+
+$('#modalEliminar').load(url, function(){
+$(this).modal('show');
+})
+}
