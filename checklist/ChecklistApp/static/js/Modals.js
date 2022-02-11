4b5df42a710899faf71aef8a5c114e208698d46c
JavierIbarraSanchez <javier.i.ibarra.s@gmail.com>
2022-02-11 16:31:55 -0300

    se  agrega aplicacion react

--- checklist/ChecklistApp/static/js/Modals.js
+++ checklist/ChecklistApp/static/js/Modals.js
@@ -1,52 +1,52 @@
 
-function abrir_modal_edit_check(url){
+function abrir_modal_edit_check(url) {
 
-    $('#editar_checklist').load(url, function(){
+    $('#editar_checklist').load(url, function () {
         $(this).modal('show');
     })
 }
 
-function abrir_modal_eliminar_check(url){
+function abrir_modal_eliminar_check(url) {
 
-$('#modalEliminar').load(url, function(){
-$(this).modal('show');
-})
-}
-
-
-function abrir_modal_crear_act(url){
-
-    $('#modalCrear').load(url, function(){
+    $('#modalEliminar').load(url, function () {
         $(this).modal('show');
     })
 }
 
 
-function abrir_modal_editar_act(url){
+function abrir_modal_crear_act(url) {
 
-    $('#modalEditar').load(url, function(){
+    $('#modalCrear').load(url, function () {
         $(this).modal('show');
     })
 }
 
-function abrir_modal_eliminar_act(url){
 
-$('#modalEliminar').load(url, function(){
-$(this).modal('show');
-})
+function abrir_modal_editar_act(url) {
+
+    $('#modalEditar').load(url, function () {
+        $(this).modal('show');
+    })
 }
 
-function abrir_modal_updateuncheck(url){
+function abrir_modal_eliminar_act(url) {
 
-    $('#modificarUncheck').load(url, function(){
+    $('#modalEliminar').load(url, function () {
+        $(this).modal('show');
+    })
+}
+
+function abrir_modal_updateuncheck(url) {
+
+    $('#modificarUncheck').load(url, function () {
         $(this).modal('show');
 
-        
-})
-}
-function abrir_modal_updatecheck(url){
 
-$('#modificarCheck').load(url, function(){
-    $(this).modal('show');
-})
+    })
+}
+function abrir_modal_updatecheck(url) {
+
+    $('#modificarCheck').load(url, function () {
+        $(this).modal('show');
+    })
 }
